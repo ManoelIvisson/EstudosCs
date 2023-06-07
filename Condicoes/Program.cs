07923ad5759fe4f59915fff7ebeef7f8c1108e31
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-06-07 10:41:06 -0300

    teste

---
+++ Condicoes/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program {
+    static void Main(string[] args) {
+        if (25 == 32) {
+            Console.WriteLine("É igual");
+        } else {
+            Console.WriteLine("É diferente");
+        }
+    }
+}
+
