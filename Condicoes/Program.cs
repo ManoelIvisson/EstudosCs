fe935d088fb08b00474a2015e28c4f042a8bdccd
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-06-07 11:00:41 -0300

    Treinando a condicinal if

--- Condicoes/Program.cs
+++ Condicoes/Program.cs
@@ -1,9 +1,13 @@
 ﻿class Program {
     static void Main(string[] args) {
-        if (25 == 32) {
-            Console.WriteLine("É igual");
+        int idade = 18;
+        int maioridade = 21;
+        int idadeMaxima = 65;
+
+        if (idade >= maioridade && idade < idadeMaxima) {
+            Console.WriteLine("É um adulto com idade abaixo de 65 anos");
         } else {
-            Console.WriteLine("É diferente");
+            Console.WriteLine("É um jovem com idade menor que 18 anos ou um idoso acima de 65");
         }
     }
 }
