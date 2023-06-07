900dca89e79640aaf954e9a5458e65ee0dd70bbe
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-06-07 11:21:25 -0300

    update

--- Condicoes/Program.cs
+++ Condicoes/Program.cs
@@ -1,13 +1,29 @@
 ﻿class Program {
     static void Main(string[] args) {
-        int idade = 18;
-        int maioridade = 21;
-        int idadeMaxima = 65;
+        // int idade = 18;
+        // int maioridade = 21;
+        // int idadeMaxima = 65;
 
-        if (idade >= maioridade && idade < idadeMaxima) {
-            Console.WriteLine("É um adulto com idade abaixo de 65 anos");
-        } else {
-            Console.WriteLine("É um jovem com idade menor que 18 anos ou um idoso acima de 65");
+        // Operador IF
+        // if (idade >= maioridade && idade < idadeMaxima) {
+        //     Console.WriteLine("É um adulto com idade abaixo de 65 anos");
+        // } else {
+        //     Console.WriteLine("É um jovem com idade menor que 18 anos ou um idoso acima de 65");
+        // }
+
+        // Operador Switch
+        string valor = "Manoel";
+
+        switch(valor) {
+            case "André":
+                Console.WriteLine("É André");
+                break;
+            case "Manoel":
+                Console.WriteLine("Sou eu HEHE");
+                break;
+            default:
+                Console.WriteLine("É Tiriricaaaa");
+                break;
         }
     }
 }
