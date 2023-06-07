72943ee58620e9a7519253621d1a206c212b3156
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-06-07 10:30:45 -0300

    modficado

--- Operadores/Program.cs
+++ Operadores/Program.cs
@@ -1,14 +1,16 @@
 ﻿class Program {
     static void Main(string[] args) {
-        int x = 0;
+        double x = 0;
         x += 5; // resultado = 5, mesma coisa que x = x + 5
         x -= 3; // resultado = 2, mesma coisa que x = x - 5
         x *= 2; // resultado = 4, mesma coisa que x = x * 5
         x /= 4; // resultado = 1, mesma coisa que x = x / 5
-
+        x++;
 
         Console.WriteLine(x);
         Console.WriteLine(x + 100 <= 100);
+        x++;
+        Console.WriteLine(x);
     }
 }
 
