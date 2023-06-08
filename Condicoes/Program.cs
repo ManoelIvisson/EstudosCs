5843e33d7a1ea68c656d1ad5d5c7492e1eb35c2d
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-06-08 18:56:05 -0300

    Terminando o módulo das bases do C#

--- Condicoes/Program.cs
+++ Condicoes/Program.cs
@@ -12,19 +12,79 @@
         // }
 
         // Operador Switch
-        string valor = "Manoel";
+        // string valor = "Manoel";
 
-        switch(valor) {
-            case "André":
-                Console.WriteLine("É André");
-                break;
-            case "Manoel":
-                Console.WriteLine("Sou eu HEHE");
-                break;
-            default:
-                Console.WriteLine("É Tiriricaaaa");
-                break;
-        }
+        // switch(valor) {
+        //     case "André":
+        //         Console.WriteLine("É André");
+        //         break;
+        //     case "Manoel":
+        //         Console.WriteLine("Sou eu HEHE");
+        //         break;
+        //     default:
+        //         Console.WriteLine("É Tiriricaaaa");
+        //         break;
+        // }
+
+
+        // Laço de repetição: For
+        // Console.Write("Digite um número: ");
+        // int numero = Convert.ToInt32(Console.ReadLine());
+        // double soma = 0;
+
+        // for (double i = 1; i <= numero; i++) {
+        //     soma += 1/i;
+        //     Console.WriteLine(soma);
+        // }
+        // Console.ReadKey();
+
+        // Laço de repetição: While
+        // int valor = 0;
+
+        // while (valor <= 5) {
+        //     valor++;
+        //     Console.WriteLine(valor);
+        //     // valor++;
+        // }
+
+        // Laço de repetição: Do/While
+        // int valor = 0;
+
+        // do {
+        //     Console.WriteLine(valor);
+        //     valor++;
+        // } while (valor < 5);
+
+        //Structs e Enumeradores
+        var produto =  new Produto(0, "Manoel", 20.0, ETipoProduto.Produto);
+
+        Console.WriteLine(produto.Id);
+        Console.WriteLine(produto.Nome);
+        Console.WriteLine(produto.Preco);
+        Console.WriteLine(produto.TipoProduto);
+        Console.WriteLine((int)produto.TipoProduto);
+
     }
+struct Produto {
+        public Produto(int id, string nome, double preco, ETipoProduto tipo) {
+            Id = id;
+            Nome = nome;
+            Preco = preco;
+            TipoProduto = tipo;
+        }
+
+        public int Id;
+        public string Nome;
+        public double Preco;
+        public ETipoProduto TipoProduto;
+
+        public double PrecoEmDolar(double dolar) {
+            return Preco * dolar;
+        }
+}
+enum ETipoProduto {
+    Servico = 1,
+    Produto = 2,
+}
 }
 
