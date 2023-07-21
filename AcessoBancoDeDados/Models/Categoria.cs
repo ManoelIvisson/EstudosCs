ef916604b80b209c0551c7885a28c09264341044
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-07-21 17:15:23 -0300

    Continuando o aprendizado

--- AcessoBancoDeDados/Models/Categoria.cs
+++ AcessoBancoDeDados/Models/Categoria.cs
@@ -2,5 +2,10 @@ namespace AcessoBancoDeDados {
     public class Categoria {
         public Guid Id { get; set; }
         public string? Titulo { get; set; }
+        public string? Resumo { get; set; }
+        public string? Url { get; set; }
+        public int Ordem { get; set; }
+        public string? Descricao { get; set; }
+        public bool Destaque { get; set; }
     } 
 }
--- AcessoBancoDeDados/Program.cs
+++ AcessoBancoDeDados/Program.cs
@@ -1,22 +1,33 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using AcessoBancoDeDados;
+using Dapper;
+using Microsoft.Data.SqlClient;
 class Program {
     static void Main(string[] args){
-        const string coneccaoString = @"Server=.\SQLEXPRESS;Database=Balta;Integrated Security=SSPI;TrustServerCertificate=True";
+        const string conexaoString = @"Server=.\SQLEXPRESS;Database=Balta;Integrated Security=SSPI;TrustServerCertificate=True";
 
-        using (var coneccao = new SqlConnection(coneccaoString)) {
-            coneccao.Open();
+        var categoria = new Categoria();
+        categoria.Id = Guid.NewGuid();
+        categoria.Titulo = "Celulares móveis";
+        categoria.Resumo = "Resumindo, celulares são móveis hoje em dia";
+        categoria.Url = "celular-movel";
+        categoria.Ordem = 2;
+        categoria.Descricao = "Muito bom esse curso";
+        categoria.Destaque = false;
+
+        var  insertSql = "INSERT INTO [Categoria](Id, Titulo, Resumo, Url, Ordem, Descricao, Destaque) VALUES (@Id, @Titulo, @Resumo, @Url, @Ordem, @Descricao, @Destaque)";
+
+        using (var conexao = new SqlConnection(conexaoString)) {
             Console.WriteLine("Conectado!");
 
-            using (var comando = new SqlCommand()) {
-                comando.Connection = coneccao;
-                comando.CommandType = System.Data.CommandType.Text;
-                comando.CommandText = "SELECT [Nome], [Email] FROM [Estudantes]";
+            var linhas = conexao.Execute(insertSql, new {categoria.Id, categoria.Titulo, categoria.Resumo, categoria.Url, categoria.Ordem, categoria.Descricao, categoria.Destaque});
 
-                var leitor = comando.ExecuteReader();
-                while(leitor.Read()) {
-                    Console.WriteLine($"{leitor.GetString(0)} - {leitor.GetString(1)}");
-                }
+            Console.WriteLine($"{linhas} linhas inseridas");
+
+            var categorias = conexao.Query<Categoria>("SELECT [Id], [Titulo], [Resumo] FROM [Categoria]");
+            foreach (var item in categorias) {
+                Console.WriteLine($"{item.Id} - {item.Titulo} - {item.Resumo}");
             }
-        }   
-    }
+        }
+    }   
 }
+
