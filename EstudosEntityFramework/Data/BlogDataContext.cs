5fcfaab53a8f54f0a230cb2663d6b9c3488af265
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-08-21 10:56:57 -0300

    Upload feito

--- EstudosEntityFramework/Data/BlogDataContext.cs
+++ EstudosEntityFramework/Data/BlogDataContext.cs
@@ -3,12 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data {
     public class BlogDataContext : DbContext {
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Perfil> Perfis { get; set; }
-        public DbSet<Postagem> Postagens { get; set; }
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Perfil>? Perfis { get; set; }
+        public DbSet<Postagem>? Postagens { get; set; }
         // public DbSet<PostagemTag>? PostagemTags { get; set; }
-        public DbSet<Tag> Tags { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tag>? Tags { get; set; }
+        public DbSet<Usuario>? Usuarios { get; set; }
         // public DbSet<UsuarioPerfil>? UsuarioPerfis { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder opcoes)
--- EstudosEntityFramework/Program.cs
+++ EstudosEntityFramework/Program.cs
@@ -4,7 +4,16 @@ using Blog.Models;
 Console.WriteLine("Hello, World!");
 
 using (var contexto = new BlogDataContext()) {
-    var tag = new Tag {Nome = "JavaSript", Slug = "javasript"};
-    contexto.Tags.Add(tag);
+    // Criar(Create)
+    // var tag = new Tag {Nome = "JavaSript", Slug = "javasript"};
+    // contexto.Tags.Add(tag);
+    // contexto.SaveChanges();
+
+    // Atualizar(Update)
+    var tag = contexto.Tags.FirstOrDefault(x => x.Id == 7)!;
+    tag.Nome = "JavaScript";
+    tag.Slug = "javascript";
+
+    contexto.Update(tag);
     contexto.SaveChanges();
 }
