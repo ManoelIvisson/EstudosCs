7193832a9c2c3d180e72adda23c34a2261aecba1
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-08-25 15:39:12 -0300

    BlogDataContext atualizado

--- EFComFluentMapping/Data/BlogDataContext.cs
+++ EFComFluentMapping/Data/BlogDataContext.cs
@@ -1,10 +1,20 @@
+using Blog.Data.Mappings;
+using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data {
     public class BlogDataContext : DbContext {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Postagem> Postagens { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder opcoes)
-        => opcoes.UseSqlServer(@"");
+        => opcoes.UseSqlServer(@"Server=.\SQLEXPRESS;Database=Blog;Integrated Security=SSPI;TrustServerCertificate=True");
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+           modelBuilder.ApplyConfiguration(new CategoriaMap());
+           modelBuilder.ApplyConfiguration(new UsuarioMap());
+           modelBuilder.ApplyConfiguration(new PostagemMap());
+        }
     }
 }
