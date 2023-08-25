57020327d5e80165e4c6825c515931437bd5ef3f
Manoel Ívisson <manoelivisson4@hotmail.com>
2023-08-25 14:20:23 -0300

    Criado o mapeamento do usuario

---
+++ EFComFluentMapping/Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,34 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings {
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario> {
+        public void Configure(EntityTypeBuilder<Usuario> builder) {
+            builder.ToTable("Usuario");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd().UseIdentityColumn();
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasColumnName("Nome")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Email);
+            builder.Property(x => x.SenhaHash);
+            builder.Property(x => x.Bio);
+            builder.Property(x => x.Imagem);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
+            
+            builder.HasIndex(x => x.Email, "IX_Usuario_Email").IsUnique();
+            builder.HasIndex(x => x.Slug, "IX_Usuario_Slug").IsUnique();
+        }
+    }
+}
